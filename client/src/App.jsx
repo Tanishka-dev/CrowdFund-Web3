3582098324a43456acbacdb003195e213d31d46c
Tanishka-dev <tanishakayadav2110@gmail.com>
2023-08-10 00:59:59 +0530

    Create Campaign page setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,16 +4,18 @@ import { Profile, CreateCampaign, Home, CampaignDetails } from "./pages";
 import { SideBar, NavBar } from "./components";
 const App = () => {
    return (
-      <div className="relative sm:8 p-4 bg-[#13131a] min-h-screen flex flex-row">
+      <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
          <div className="sm:flex hidden mr-10 relative ">
             <SideBar />
          </div>
          <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
             <NavBar />
+
+            <Routes>
+               <Route path="/" element={<Home />} />
+               <Route path="/create-campaign" element={<CreateCampaign />} />
+            </Routes>
          </div>
-         <Routes>
-            <Route path="/" />
-         </Routes>
       </div>
    );
 };
--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -1,7 +1,30 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { ethers } from "ethers";
+import { money } from "../assets";
+import { checkIfImage } from "../utils";
+import { CustomButton } from "../components";
 const CreateCampaign = () => {
-   return <div>CreateCampaign</div>;
+   const navigate = useNavigate();
+   const [isLoading, setisLoading] = useState(false);
+   const [form, setform] = useState({
+      name: "",
+      deadline: "",
+      title: "",
+      target: "",
+      description: "",
+      image: "",
+   });
+   return (
+      <div className="bg-[#1c1c24] flex justify-center items-center flex-col rounded-[10px] sm:p-4">
+         {isLoading && "Loader.."}
+         <div className="bg-[#3a3a43] flex justify-center items-center p-[16px] sm:min-w-[380px] rounded-[10px]">
+            <h1 className="text-white font-epilogue font-bold text-[18px] sm:text-[25px] leading-[38px] ">
+               Start a Campaign 🚀
+            </h1>
+         </div>
+      </div>
+   );
 };
 
 export default CreateCampaign;
