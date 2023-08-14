8c66035f1540927280c66a210f559df519d8dc98
Tanishka-dev <tanishakayadav2110@gmail.com>
2023-08-15 03:15:09 +0530

    Campaign Details Setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,6 +15,10 @@ const App = () => {
                <Route path="/" element={<Home />} />
                <Route path="/profile" element={<Profile />} />
                <Route path="/create-campaign" element={<CreateCampaign />} />
+               <Route
+                  path="/campaign-details/:id"
+                  element={<CampaignDetails />}
+               />
             </Routes>
          </div>
       </div>
--- client/src/pages/CampaignDetails.jsx
+++ client/src/pages/CampaignDetails.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { ethers } from "ethers";
+import { thirdweb } from "../assets";
+import { daysLeft, calculateBarPercentage } from "../utils";
+import { useLocation } from "react-router-dom";
+import { useStateContext } from "../context";
+import { CustomButton } from "../components";
 
 const CampaignDetails = () => {
-   return <div>CampaignDetails</div>;
+   const [isLoading, setisLoading] = useState(false);
+   const [amount, setamount] = useState("");
+   const [donators, setdonators] = useState([]);
+   const state = useLocation();
+   const { getDonations, address, contract } = useStateContext();
+   const remainingDays = daysLeft(state.deadline);
+   return <div>Loading</div>;
 };
 
 export default CampaignDetails;
